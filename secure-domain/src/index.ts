8595e82614d8c3e6f620938bc3a4f7cedd32c2ae
Brendan Arnold <brendanarnold@gmail.com>
2020-11-21 22:25:13 +0000

    Basic Express app running

--- secure-domain/src/index.ts
+++ secure-domain/src/index.ts
@@ -1,2 +1,21 @@
-let message: string = 'Hello World';
-console.log(message);
+
+import * as express from 'express'
+import { AddressInfo } from 'net'
+import * as pkg from '../package.json'
+
+const app = express()
+
+app.get('/info/version', (req, res) => {
+  const version = pkg.version
+  res.json({ version })
+})
+
+app.get('/info/status-check', (req, res) => {
+  res.sendStatus(204)
+})
+
+const server = app.listen(8080, () => {
+  const { address }  = server.address() as AddressInfo
+  const { port } = server.address() as AddressInfo
+  console.log("Example app listening at http://%s:%s", address, port)
+})
