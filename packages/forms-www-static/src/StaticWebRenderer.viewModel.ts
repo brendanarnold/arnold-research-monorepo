590e66b1ad66116f39f511fa6ed43dd9655ea6a7
Brendan Arnold <brendanarnold@gmail.com>
2021-02-05 00:16:13 +0000

    ViewModels built out

--- packages/forms-www-static/src/StaticWebRenderer.viewModel.ts
+++ packages/forms-www-static/src/StaticWebRenderer.viewModel.ts
@@ -1,17 +1,55 @@
-import { FieldSet, Form } from "@tngbl/forms"
+import { Field, FieldSet, Form } from "@tngbl/forms"
 
 
 class SectionViewModel {
+  name: string
+  viewType: string
+  label: string
+  components: (SubSectionViewModel | FieldViewModel)[]
+
   static fromFieldSet (fieldSet: FieldSet): SectionViewModel {
     const viewModel = new SectionViewModel()
-    // @todo
+    viewModel.viewType = 'Section'
+    viewModel.name = fieldSet.name
+    viewModel.label = fieldSet.label
+    viewModel.components = fieldSet.structure.map(item => item instanceof Field
+      ? FieldViewModel.fromField(item)
+      : SubSectionViewModel.fromFieldSet(item))
     return viewModel
   }
 }
 
-class SubSectionViewModel {}
+class SubSectionViewModel {
+  name: string
+  viewType: string
+  label: string
+  components: FieldViewModel[]
 
-class FieldViewModel {}
+  static fromFieldSet (fieldSet: FieldSet): SubSectionViewModel {
+    const viewModel = new SubSectionViewModel()
+    viewModel.viewType = 'SubSection'
+    viewModel.name = fieldSet.name
+    viewModel.label = fieldSet.label
+    // @fixme Only go two levels deep in terms of sections, put in a check for this
+    viewModel.components = fieldSet.structure.map(field => FieldViewModel.fromField(field as Field))
+    return viewModel
+  }
+}
+
+class FieldViewModel {
+  name: string
+  type: string
+  viewType: string
+  label: string
+
+  static fromField (field: Field): FieldViewModel {
+    const viewModel = new FieldViewModel()
+    viewModel.name = field.name
+    viewModel.viewType = field.viewType
+    viewModel.label = field.label
+    return viewModel
+  }
+}
 
 export class FormViewModel {
 
@@ -19,7 +57,9 @@ export class FormViewModel {
   
   static fromForm(form: Form): FormViewModel {
     const viewModel = new FormViewModel()
-    
+    viewModel.components = form.schema.structure.map(item => item instanceof FieldSet
+      ? SectionViewModel.fromFieldSet(item as FieldSet)
+      : FieldViewModel.fromField(item as Field))
     return viewModel
   }
 }
